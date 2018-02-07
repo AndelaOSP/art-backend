139ea6649a50988fa0dd6b210d59e8e6225b2e04
Awinja <ingari61@gmail.com>
2018-02-07 16:31:55 +0300

    feature(models-setup): Setup models and application wireframe

--- art/api/views.py
+++ art/api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Asset, CheckinLogs, CheckoutLogs
+from art.serializers import AssetSerializer, CheckinLogsSerializer, CheckoutLogsSerializer
 
-# Create your views here.
+class AssetCreateApiView(ListCreateAPIView):
+    """Creates, lists item using user_id """
+    
+    serializer_class = AssetSerializer
+    # To Add code with Josiah
+
+class AssetGetUpdateDelete(RetrieveUpdateDestroyAPIView):
+    """ This gets, updates, deletes an asset using  the userid"""
+    serializer_class = AssetSerializer
+    # To Add code with Josiah
+
+"""Checkin activity logic"""
+
+class CheckinCreateApiView(ListCreateAPIView):
+    """This creates, lists chekin entries"""
+    serializer_class = CheckinLogsSerializer
+    # To Add code with Josiah
+
+
+"""checkout activity logic"""
+
+class CheckoutCreateApiView(ListCreateAPIView):
+    """This creates, lists chekin entries"""
+    serializer_class = CheckoutLogsSerializer
+    # To Add code with Josiah
