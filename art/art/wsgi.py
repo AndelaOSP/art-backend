1287b8adc833c98d71bbb5b5d752f3df973b097b
Mnickii <caroline.nkirote@andela.com>
2018-02-20 13:01:02 +0300

    ch(setup-circleci): Add circle configurations

--- art/art/wsgi.py
+++ art/art/wsgi.py
@@ -15,7 +15,8 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
-dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+dotenv.read_dotenv(os.path.join(
+  os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "art.settings")
 
