5d9da0097d2ddc622ccada21e7503cd0aa438f9a
Josiah Mokobo Nyarega <josiah.mokobo@andela.com>
2018-02-27 14:57:32 +0300

    feat(item-number-model): Create item number model
       - Create item number model
       - Add tests to the item number model
    
    [Delivers #155322118]

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import AssetCategory, AssetType, AssetSubCategory, Item, AssetMake
-
-admin.site.register(
-    [AssetCategory, AssetType, AssetSubCategory, Item, AssetMake]
+from .models import (AssetCategory, AssetType,
+                     AssetSubCategory, Item, AssetMake, ItemModelNumber)
+admin.register(
+    [
+        AssetCategory,
+        AssetType,
+        AssetSubCategory,
+        Item,
+        AssetMake,
+        ItemModelNumber
+    ]
 )
