e6d140341cab522fac1e796eddba80d1a86a1a03
egichuri <eric.gichuri@andela.com>
2018-03-01 16:28:41 +0300

    fix admin model register

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import AssetCategory, AssetType, AssetSubCategory, Item, AssetMake
 
-admin.register(
+admin.site.register(
     [AssetCategory, AssetType, AssetSubCategory, Item, AssetMake]
 )
