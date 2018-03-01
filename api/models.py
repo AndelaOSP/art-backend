90336c11c65c959553700dc809a3a9b2909e4809
Caroline Wanjiku <caroline.wanjiku@andela.com>
2018-03-01 15:03:31 +0300

    Create Item Model #155322123 (#19)
    * ft(item)Add item model #155322118
    - Write Item model tests
    - Create Item model
    
    * ft(item)Add item_code and serial_number fields
    - Validate that either item_code or serial_number is provided
    - Adjust tests
    
    * ft(items)Register Items model
    - Register Item model in `admin.py`
    - Correct path in `wsgi.py`
    - Add conditional statement to `Item.clean` method
    
    * fix circle

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 
 class AssetCategory(models.Model):
@@ -16,7 +17,7 @@ class AssetCategory(models.Model):
 
 class AssetType(models.Model):
     """Stores all asset types"""
-    asset_type = models.CharField(max_length=500)
+    asset_type = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     last_modified = models.DateTimeField(auto_now=True, editable=False)
 
@@ -32,6 +33,29 @@ class AssetMake(models.Model):
 
     def __str__(self):
         return self.make_label
+
+
+class Item(models.Model):
+    """Stores all items"""
+    item_code = models.CharField(max_length=50, blank=True)
+    serial_number = models.CharField(max_length=50, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    last_modified = models.DateTimeField(auto_now=True, editable=False)
+
+    def clean(self):
+        if not self.item_code and not self.serial_number:
+            raise ValidationError(('Please provide either the serial number,\
+                               asset code or both.'), code='required')
+
+    def save(self, *args, **kwargs):
+        """Validate either item code or serial number are provided"""
+        if not self.item_code and not self.serial_number:
+            self.full_clean()
+        else:
+            super(Item, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return '{}{}'.format(self.item_code, self.serial_number)
 
 
 class AssetSubCategory(models.Model):
