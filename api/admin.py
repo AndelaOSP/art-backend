4da3dee545ce7801ab5c27901b41725346461b45
Nelson Mandela <nelson.matagaro@andela.com>
2018-03-01 11:21:30 +0300

    Ft asset sub category #155322115 (#22)
    * Add asset sub category
    
    * Add asset sub category model
    
    * Add asset sub category model tests
    
    * Add asset sub category model migrations
    
    * chore(asset-sub-category): fix pep8 errors
    
    * chore(test coverage): Add asset sub category test

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import AssetMake
-from .models import AssetCategory, AssetType
+from .models import AssetCategory, AssetType, AssetSubCategory
 
 admin.site.register(AssetType)
 admin.site.register(AssetCategory)
 admin.site.register(AssetMake)
+admin.site.register(AssetSubCategory)
