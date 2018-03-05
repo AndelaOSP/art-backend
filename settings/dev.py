491bf161097529396c64790a8d71ca0633f65af8
Caroline Wanjiku <Caroline Wanjiku>
2018-03-05 15:49:39 +0300

    ft(debug)Django debug toolbar - Pip install package - Add configurations to settings and url

--- settings/dev.py
+++ settings/dev.py
@@ -2,7 +2,17 @@ from .base import *  # noqa: F403,F401
 
 DEBUG = True
 
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+
 ALLOWED_HOSTS = [
+    '0.0.0.0',
+    '127.0.0.1'
+]
+
+INTERNAL_IPS = [
     '0.0.0.0',
     '127.0.0.1'
 ]
