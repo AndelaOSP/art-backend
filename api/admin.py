444e52560466c7484fc78c0019413bd83e6a1413
iankigen <ian.gabe.ian@gmail.com>
2018-03-07 19:49:28 +0300

    feat(users): admin can add users
    - add default custom auth model
    - enable admin to add users
    - enable admin to see users on the admin dashboard
    [delivers ##155512028]

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from .forms import UserChangeForm, UserCreationForm
 from .models import (AssetCategory, AssetType,
                      AssetSubCategory, Item, AssetMake, ItemModelNumber)
+
+User = get_user_model()
+
 admin.site.register(
     [
         AssetCategory,
@@ -11,3 +18,42 @@ admin.site.register(
         ItemModelNumber
     ]
 )
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = (
+        'email', 'name', 'cohort', 'slack_handle', 'admin'
+    )
+    list_filter = (
+        'cohort', 'admin'
+    )
+
+    fieldsets = (
+        ('Account', {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name', 'cohort',
+                                      'slack_handle',
+                                      'picture',
+                                      'phone_number')}),
+        ('Permissions', {'fields': ('admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name',
+                       'cohort', 'slack_handle',
+                       'password1', 'password2')
+        }),
+    )
+
+    ordering = (
+        'email', 'name', 'cohort', 'slack_handle'
+    )
+    filter_horizontal = ()
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 class AssetCategory(models.Model):
@@ -89,3 +90,104 @@ class Item(models.Model):
     def __str__(self):
         return '{}{}{}'.format(self.item_code, self.serial_number,
                                self.model_number)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, cohort, slack_handle,
+                    is_active=True, is_admin=False, is_staff=False,
+                    phone_number=None, picture=None, password=None):
+        if not email:
+            raise ValueError("Email address is required")
+        elif not name:
+            raise ValueError("Name is required")
+        elif not cohort:
+            raise ValueError("Cohort is required")
+        elif not slack_handle:
+            raise ValueError("Slack handle is required")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            cohort=cohort,
+            slack_handle=slack_handle,
+        )
+
+        user.set_password(password)
+        user.picture = picture
+        user.staff = is_staff
+        user.admin = is_admin
+        user.active = is_active
+        user.phone_number = phone_number
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, name, cohort,
+                         slack_handle, password=None):
+        user = self.create_user(
+            email=email,
+            name=name,
+            cohort=cohort,
+            slack_handle=slack_handle,
+            password=password,
+            is_staff=True,
+        )
+        return user
+
+    def create_superuser(self, email, name, cohort,
+                         slack_handle, password=None):
+        user = self.create_user(
+            email=email,
+            name=name,
+            cohort=cohort,
+            slack_handle=slack_handle,
+            password=password,
+            is_staff=True,
+            is_admin=True
+        )
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=50, unique=True)
+    name = models.CharField(max_length=50, blank=True, null=True)
+    cohort = models.IntegerField(blank=True, null=True)
+    slack_handle = models.CharField(max_length=50, unique=True,
+                                    blank=True, null=True)
+    picture = models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=50, blank=True, null=True)
+    admin = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    last_modified = models.DateTimeField(auto_now=True, editable=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'cohort', 'slack_handle']
+    objects = UserManager()
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_active(self):
+        return self.active
+
+    def __str__(self):
+        return self.email
