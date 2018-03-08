e27eb556fe2708ba92c19d6395c627e0a49a9d47
iankigen <ian.gabe.ian@gmail.com>
2018-03-08 16:33:07 +0300

    fix(test_user): add user tests - add more test for use [delivers #155512028]

--- api/tests/test_user.py
+++ api/tests/test_user.py
@@ -13,11 +13,15 @@ class UserTestCase(TestCase):
 
     def test_can_add_user(self):
         users_count_before = User.objects.count()
-        User.objects.create(
+        new_user = User.objects.create(
             email='test-1@site.com', cohort=20,
             slack_handle='@test_user-1', password='devpassword'
         )
         users_count_after = User.objects.count()
+        self.assertEqual(new_user.email, 'test-1@site.com')
+        self.assertEqual(new_user.cohort, 20)
+        self.assertEqual(new_user.slack_handle, '@test_user-1')
+        self.assertEqual(new_user.password, 'devpassword')
         self.assertEqual(users_count_before, users_count_after - 1)
 
     def test_can_update_user(self):
@@ -80,3 +84,19 @@ class UserTestCase(TestCase):
         )
         self.assertTrue(new_user_1.is_staff)
         self.assertTrue(new_user_1.is_superuser)
+
+    def test_create_superuser_with_staff_false(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='test-2@site.com', cohort=20,
+                slack_handle='@test_user-2', password='devpassword',
+                is_staff=False, is_superuser=True
+            )
+
+    def test_create_superuser_with_superuser_false(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='test-2@site.com', cohort=20,
+                slack_handle='@test_user-2', password='devpassword',
+                is_staff=True, is_superuser=False
+            )
