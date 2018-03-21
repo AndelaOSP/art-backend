31827694534339493124d8dd3d842a91f666bc80
HawiCaesar <brian.odhiambo932@gmail.com>
2018-03-21 11:43:49 +0300

    chore(security-team): remove unused imports

--- api/models.py
+++ api/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 
 
 class AssetCategory(models.Model):
