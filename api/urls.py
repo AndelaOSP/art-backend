ee402735b1eb81b4088569b7e257a8168882b320
HawiCaesar <brian.odhiambo932@gmail.com>
2018-04-11 18:50:46 +0300

    chore(api-users): fix flake8 violation

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,6 @@ from .views import UserViewSet, ItemViewSet, SecurityUserEmailsViewSet
 router = SimpleRouter()
 router.register('users', UserViewSet)
 router.register('items', ItemViewSet, 'items')
-router.register('security_user_emails', SecurityUserEmailsViewSet, 'security_user_emails')
+router.register('security_user_emails',
+                SecurityUserEmailsViewSet, 'security_user_emails')
 urlpatterns = router.urls
