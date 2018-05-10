d4eb8b7e9ed7ffa21366a925c2c1b513aa8b124f
nelmandela <nelsonmandela784@gmail.com>
2018-05-10 14:36:55 +0300

    feat(asset category): Create and view asset categories via API   •add asset category endpoint   •add asset category viewset   •add asset category serializer

--- api/views.py
+++ api/views.py
@@ -6,11 +6,11 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 from api.authentication import FirebaseTokenAuthentication
 from core.models import Asset, SecurityUser, AssetLog, UserFeedback, \
-    AssetStatus, AllocationHistory
+    AssetStatus, AllocationHistory, AssetCategory
 from .serializers import UserSerializer, \
     AssetSerializer, SecurityUserEmailsSerializer, \
     AssetLogSerializer, UserFeedbackSerializer, \
-    AssetStatusSerializer, AllocationsSerializer
+    AssetStatusSerializer, AllocationsSerializer, AssetCategorySerializer
 from api.permissions import IsApiUser, IsSecurityUser
 
 User = get_user_model()
@@ -93,4 +93,12 @@ class AllocationsViewSet(ModelViewSet):
     queryset = AllocationHistory.objects.all()
     permission_classes = [IsAuthenticated, ]
     authentication_classes = (FirebaseTokenAuthentication,)
+    http_method_names = ['get', 'post']
+
+
+class AssetCategoryViewSet(ModelViewSet):
+    serializer_class = AssetCategorySerializer
+    queryset = AssetCategory.objects.all()
+    permission_classes = [IsAuthenticated]
+    authentication_classes = (FirebaseTokenAuthentication)
     http_method_names = ['get', 'post']
