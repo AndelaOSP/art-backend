01723bf3ffe1608dbfbd1005e083826fcd63ce00
Joan Awinja Ingari <jus_machungwa@MacBook-Pro-76.local>
2018-05-16 17:18:25 +0300

    75% working tests

--- api/tests/test_asset_condition_api.py
+++ api/tests/test_asset_condition_api.py
@@ -6,10 +6,7 @@ from rest_framework.reverse import reverse
 
 from core.models import (Asset,
                          AssetModelNumber,
-                         AssetMake,
-                         AssetType,
-                         AssetSubCategory,
-                         AssetCategory)
+                         AssetCondition)
 
 User = get_user_model()
 client = APIClient()
@@ -23,53 +20,145 @@ class AssetConditionAPITest(TestCase):
             email='testuser@gmail.com', cohort=19,
             slack_handle='tester', password='qwerty123'
         )
-        self.asset_category = AssetCategory.objects.create(
-            category_name="Accessories")
-        self.asset_sub_category = AssetSubCategory.objects.create(
-            sub_category_name="Sub Category name",
-            asset_category=self.asset_category)
-        self.asset_type = AssetType.objects.create(
-            asset_type="Asset Type",
-            asset_sub_category=self.asset_sub_category)
-        self.make_label = AssetMake.objects.create(
-            make_label="Asset Make", asset_type=self.asset_type)
-        self.assetmodel = AssetModelNumber(
-            model_number="IMN50987", make_label=self.make_label)
+        self.assetmodel = AssetModelNumber(model_number="IMN50987")
         self.assetmodel.save()
-        self.asset = Asset(
+        self.token_user = 'testtoken'
+
+        self.test_asset = Asset(
             asset_code="IC001",
             serial_number="SN001",
             assigned_to=self.user,
             model_number=self.assetmodel,
         )
-        self.asset.save()
+        self.test_asset.save()
+        self.asset = Asset.objects.get(serial_number="SN001")
+        self.asset_condition = AssetCondition(
+                                    asset=self.asset,
+                                    asset_condition="working"
+                                    )
+        self.asset_condition.save()
+        self.asset_conditon = AssetCondition.objects.get(asset=self.asset)
+        # print("asset condition", self.asset_conditon)
+        self.asset_condition_urls = reverse('asset-condition-list')
 
-        self.condition_url = reverse('asset-condition-list')
-        self.token_user = 'testtoken'
-
-    @patch('api.authentication.auth.verify_id_token')
-    def test_can_post_condition(self, mock_verify_token):
-        pass
-
-    @patch('api.authentication.auth.verify_id_token')
-    def test_can_get_all_condition(self, mock_verify_token):
-        pass
-
-    @patch('api.authentication.auth.verify_id_token')
-    def test_can_get_single_condition(self, mock_verify_token):
-        pass
+    def test_non_authenciated_user_can_view_asset_condition(self):
+        response = client.get(self.asset_condition_urls)
+        self.assertEqual(response.status_code, 401)
 
     @patch('api.authentication.auth.verify_id_token')
-    def test_condition_api_endpoint_cant_allow_put(
+    def test_authenciated_user_can_view_asset_condition(
                             self, mock_verify_id_token):
-        pass
+        mock_verify_id_token.return_value = {'email': self.user.email}
+        response = client.get(
+            self.asset_condition_urls,
+            HTTP_AUTHORIZATION="Token {}".format(self.token_user))
+        self.assertIn(self.asset_condition.asset_condition,
+                      response.data[0].values())
+        self.assertEqual(len(response.data), Asset.objects.count())
+        self.assertEqual(response.status_code, 200)
 
     @patch('api.authentication.auth.verify_id_token')
-    def test_condition_api_endpoint_cant_allow_patch(
-                            self, mock_verify_id_token):
-        pass
+    def test_authenticated_user_can_post_asset_condition(self,
+                                                         mock_verify_id_token):
+        mock_verify_id_token.return_value = {'email': self.user.email}
+        test_asset = Asset(
+            asset_code="IC002",
+            serial_number="SN002",
+            assigned_to=self.user,
+            model_number=self.assetmodel,
+        )
+        test_asset.save()
+        data = {
+            "asset": test_asset.serial_number,
+            "asset_condition": "working perfectly"
+        }
+        response = client.post(
+            self.asset_condition_urls,
+            data=data,
+            HTTP_AUTHORIZATION="Token {}".format(self.token_user))
+        self.assertIn(data['asset_condition'], response.data.values())
+        self.assertEqual(response.status_code, 201)
 
     @patch('api.authentication.auth.verify_id_token')
-    def test_condition_api_endpoint_cant_allow_delete(
+    def test_authenticated_user_cant_post_invalid_asset_serial_number(
                             self, mock_verify_id_token):
-        pass
+        mock_verify_id_token.return_value = {'email': self.user.email}
+        invalid_asset = Asset(
+            asset_code="IC0024",
+            serial_number="SN0014",
+            assigned_to=self.user,
+            model_number=self.assetmodel,
+        )
+        data = {
+            "asset": invalid_asset,
+            "asset_condition": "working perfectly"
+        }
+        response = client.post(
+            self.asset_condition_urls,
+            data=data,
+            HTTP_AUTHORIZATION="Token {}".format(self.token_user))
+        self.assertEqual(response.status_code, 400)
+
+    @patch('api.authentication.auth.verify_id_token')
+    def test_authenticated_user_can_get_all_asset_condition(
+                                self, mock_verify_id_token):
+        mock_verify_id_token.return_value = {'email': self.user.email}
+        response = client.get(
+                self.asset_condition_urls,
+                HTTP_AUTHORIZATION="Token {}".format(self.token_user))
+        self.assertEqual(response.status_code, 200)
+
+    @patch('api.authentication.auth.verify_id_token')
+    def test_authenticated_user_can_get_single_asset_condition(
+                                    self, mock_verify_id_token):
+        mock_verify_id_token.return_value = {'email': self.user.email}
+        test_asset = Asset(
+            asset_code="IC002",
+            serial_number="SN002",
+            assigned_to=self.user,
+            model_number=self.assetmodel,
+        )
+        test_asset.save()
+        data = {
+            "asset": test_asset.serial_number,
+            "asset_condition": "working perfectly"
+        }
+        new_asset_condition = client.post(
+            self.asset_condition_urls,
+            data=data,
+            HTTP_AUTHORIZATION="Token {}".format(self.token_user))
+        self.assertEqual(new_asset_condition.status_code, 201)
+        response = client.get(self.asset_condition_urls, '1/',
+                              HTTP_AUTHORIZATION="Token {}"
+                              .format(self.token_user))
+        self.assertEqual(response.data, {
+             "id": '1'
+        })
+        self.assertEqual(response.status_code, 200)
+
+    @patch('api.authentication.auth.verify_id_token')
+    def test_authenticated_user_condition_api_endpoint_cannot_allow_put(
+                            self, mock_verify_id_token):
+        mock_verify_id_token.return_value = {'email': self.user.email}
+        response = client.put(
+            self.asset_condition_urls,
+            HTTP_AUTHORIZATION="Token {}".format(self.token_user))
+        self.assertEqual(response.status_code, 405)
+
+    @patch('api.authentication.auth.verify_id_token')
+    def test_authenticated_user_condition_api_endpoint_cannot_allow_patch(
+                            self, mock_verify_id_token):
+        mock_verify_id_token.return_value = {'email': self.user.email}
+        response = client.patch(
+            self.asset_condition_urls,
+            HTTP_AUTHORIZATION="Token {}".format(self.token_user))
+        self.assertEqual(response.status_code, 405)
+
+    @patch('api.authentication.auth.verify_id_token')
+    def test_condition_api_endpoint_cannot_allow_delete(
+                            self, mock_verify_id_token):
+        mock_verify_id_token.return_value = {'email': self.user.email}
+        response = client.delete(
+            self.asset_condition_urls,
+            HTTP_AUTHORIZATION="Token {}".format(self.token_user))
+        self.assertEqual(response.status_code, 405)
