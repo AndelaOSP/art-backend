6268416b2648a7267df43ca898f5e428f51ec6cc
egichuri <eric.gichuri@andela.com>
2018-05-30 15:28:54 +0300

    [157681976] enable live testing on API docs

--- api/urls.py
+++ api/urls.py
@@ -45,8 +45,10 @@ router.register('asset-makes', AssetMakeViewSet, 'asset-makes')
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('docs/', schema_view.with_ui('redoc',
-                                      cache_timeout=None), name='schema-redoc')
+    path('docs/', schema_view.with_ui(
+        'redoc', cache_timeout=None), name='schema-redoc'),
+    path('docs/live/', schema_view.with_ui(
+        'swagger', cache_timeout=None), name='schema-swagger')
 ]
 
 urlpatterns += router.urls
