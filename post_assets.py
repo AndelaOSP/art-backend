10d7da91023f741f4f7ba47a072518f0b5e323ee
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2018-06-07 14:43:53 +0100

    ft(asset) implement batch insert assets into system
    - add an import script
    
    [Finishes #155922725]

--- post_assets.py
+++ post_assets.py
@@ -2,25 +2,22 @@ import sys
 import os
 import csv
 import django
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
+
 project_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(project_dir)
 django.setup()
-from core.models.asset import Asset, AssetModelNumber
+from core.models.asset import Asset, AssetModelNumber   # noqa
 
 
 def bulk_create_asset():
-    result = dict()
-    error = dict()
     with open('assets.csv', 'r', ) as f:
-        error['detail'] = []
+        skipped = dict()
         skipped_records = []
         inserted_records = []
         data = csv.DictReader(f, delimiter='|')
         for row in data:
             model_number = AssetModelNumber.objects.\
-                filter(pk=row['model_number'])\
+                filter(model_number=row['model_number'])\
                 .exists()
             asset_code = Asset.objects.filter(asset_code=row['asset_code'])\
                 .exists()
@@ -28,29 +25,50 @@ def bulk_create_asset():
                 serial_number=row['serial_number']).exists()
             if asset_code:
                 skipped_records.append(row)
-                error['detail'].append('asset_code {0} already exist'.
-                                       format(row['asset_code']))
+                skipped[row['asset_code']] = ('asset_code {0} already exists'.
+                                              format(row['asset_code']))
             elif serial_number:
                 skipped_records.append(row)
-                error['detail'].append('serial_number {0} already exist'.
-                                       format(row['serial_number']))
-                # raise ValidationError(_('asset_code and (or) serial'
-                #                         ' number already exist '))
+                skipped[row['serial_number']] = ('serial_number {0} '
+                                                 'already exists'.
+                                                 format(row['serial_number']))
             elif not model_number:
                 skipped_records.append(row)
-                error['detail'].append('model number {0} does not exist'.
-                                       format(row['model_number']))
+                skipped[row['model_number']] = ('model number {0} does '
+                                                'not exist'.format
+                                                (row['model_number']))
             else:
                 asset = Asset()
                 asset.asset_code = row['asset_code']
                 asset.serial_number = row['serial_number']
                 asset.model_number = AssetModelNumber.objects.\
-                    get(pk=row['model_number'])
+                    get(model_number=row['model_number'])
                 asset.save()
                 inserted_records.append(asset)
-    result['inserted_records'] = inserted_records
-    result['skipped_records'] = skipped_records
-    return result, error
+    display_inserted(inserted_records)
+    display_skipped(skipped)
+
+
+def display_inserted(result):
+    print('***************************************************************\n')
+    print("There are {0}  successfully inserted records\n".format(len(result)))
+    print('===============================================================\n')
+    if len(result) <= 0:
+        print("No record was inserted\n")
+    else:
+        for item in result:
+            print("\t\t{0}".format(item))
+
+
+def display_skipped(result):
+    print('***************************************************************\n')
+    print("There are {0}  skipped records \n".format(len(result)))
+    print('===============================================================\n')
+    if len(result) <= 0:
+        print("No record was skipped \n")
+    else:
+        for key, value in result.items():
+            print("{0} : \t\t{1}".format(key, value))
 
 
 if __name__ == '__main__':
