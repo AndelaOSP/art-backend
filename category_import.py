70d64733dd550da3cd09f2e41887029ebdc660ee
abdulquddus <qudus.yekeen@andela.com>
2018-06-09 11:18:23 +0100

    feat(assets): Fix merge conflicts - [finishes -#155922722]

--- category_import.py
+++ category_import.py
@@ -1,7 +1,8 @@
 import sys
 import os
+import csv
+from tqdm import tqdm
 import django
-import pandas as pd
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(project_dir)
@@ -11,56 +12,128 @@ django.setup()
 from core.models.asset import AssetCategory, AssetSubCategory  # noqa
 
 
-def create_category(x):
-    filter_category = AssetCategory.objects.filter(category_name=x).exists()
-    if filter_category:
-        return 'Category exist'
-    else:
-        add_category = AssetCategory.objects.create(category_name=x)
-        add_category.save()
-        return 'Category created'
-
-
-def create_subcategory(z):
-    category, subcategory = z.split(',')[0], z.split(',')[1]
-    filter_subcategory = AssetSubCategory.objects.filter(
-        sub_category_name=subcategory).exists()
-    if filter_subcategory:
-        return 'Sub category exist'
-    else:
-        get_category = AssetCategory.objects.get(category_name=category)
-        add_sub_category = AssetSubCategory.objects.create(
-            sub_category_name=subcategory,
-            asset_category=get_category
-        )
-        add_sub_category.save()
-        return 'Sub category created'
-
-
-def csv_file(file):
-    if os.path.isfile(file):
-        if not file.endswith('.csv'):
+def load_csv_file(file_name):  # noqa
+    print('\n')
+    if os.path.isfile(file_name):
+        if not file_name.endswith('.csv'):
             return 'File not supported'
+
         else:
-            rd_csv = pd.read_csv(file)
+            with open(file_name, 'r', ) as f:
+                file_length = len(f.readlines()) - 1
+                f.seek(0)
+                skipped = dict()
+                inserted_records = []
+                data = csv.DictReader(f, delimiter=',')
+                counter = 1
+                with tqdm(total=file_length) as pbar:
+                    for row in data:
+                        assets_category = AssetCategory.objects.\
+                            filter(category_name=row['Category']).exists()
+                        assets_sub_category = AssetSubCategory.objects.\
+                            filter(sub_category_name=row['Sub Category']).\
+                            exists()
+                        if assets_category:
+                            if assets_sub_category:
+                                skipped[row['Category']] = [(
+                                    'Category {0} already exists'.
+                                    format(row['Category'])), counter]
+                                skipped[row['Sub Category']] = [(
+                                    'Sub Category {0} already exists'.
+                                    format(row['Sub Category'])), counter]
+                            else:
+                                category = AssetCategory.objects.get(
+                                    category_name=row['Category']
+                                )
+                                assets_sub_category = AssetSubCategory.\
+                                    objects.create(
+                                        sub_category_name=row['Sub Category'],
+                                        asset_category=category
+                                    )
+                                assets_sub_category.save()
+                                inserted_records.append([
+                                    assets_sub_category,
+                                    counter]
+                                )
+                                skipped[row['Category']] = [(
+                                    'Category {0} already exists'.
+                                    format(row['Category'])), counter]
 
-            def check_if_category_exist(x):
-                return create_category(x)
+                        elif not assets_category:
+                            if assets_sub_category:
+                                asset_category = AssetCategory.objects.create(
+                                    category_name=row['Category']
+                                )
+                                asset_category.save()
+                                inserted_records.append([
+                                    asset_category,
+                                    counter]
+                                )
+                                skipped[row['Sub Category']] = [(
+                                    'Sub Category {0} already exists'.
+                                    format(row['Sub Category'])), counter]
+                            else:
+                                asset_category = AssetCategory.objects.create(
+                                    category_name=row['Category']
+                                )
+                                asset_category.save()
+                                category = AssetCategory.objects.get(
+                                    category_name=row['Category']
+                                )
+                                sub_category = AssetSubCategory.objects.create(
+                                    sub_category_name=row['Sub Category'],
+                                    asset_category=category
+                                )
+                                sub_category.save()
+                                inserted_records.append([
+                                    asset_category,
+                                    counter]
+                                )
+                                inserted_records.append([
+                                    sub_category,
+                                    counter]
+                                )
 
-            def check_if_sub_category_exist(y):
-                return create_subcategory(y)
+                        counter += 1
+                        pbar.update(1)
+            print("\n")
+            display_inserted(inserted_records)
+            display_skipped(skipped)
 
-            rd_csv['Category Summary'] = (rd_csv.Category).\
-                apply(check_if_category_exist)
-            rd_csv['SubCategory'] = rd_csv.Category.apply(lambda x: x) \
-                + ',' + rd_csv['Sub Category'].apply(lambda x: x)
-            rd_csv['Subcategory Summary'] = rd_csv.SubCategory.apply(
-                check_if_sub_category_exist)
-            return rd_csv.drop('SubCategory', 1)
     return 'File not found'
+
+
+def display_inserted(result):
+    print('----------------------------------------------------------------\n')
+    print('        --------    TRANSACTIONS SUMMARY        --------        \n')
+    print('----------------------------------------------------------------\n')
+    print("There are {0}  successfully inserted records\n".format(len(result)))
+    print('================================================================\n')
+    if len(result) <= 0:
+        print("No record was inserted\n")
+    else:
+        print('{0} \t{1} \t '.format('Line No.', 'Description'))
+        print('------------------------------------------------------------\n')
+        for item in result:
+            print('{0}\t{1}'.format(item[1], item[0]))
+
+
+def display_skipped(result):
+    print('----------------------------------------------------------------\n')
+    print('There are {0}  skipped records \n'.format(len(result)))
+    print('===============================================================\n')
+    if len(result) <= 0:
+        print('No record was skipped \n')
+    else:
+        print('{0} \t{1} \t {2}'.format('Line No.', 'Description',
+                                        'Error Message'))
+        print('------------------------------------------------------------\n')
+        for key, value in result.items():
+
+            print('{0}\t \t{1} \t{2}'.format(value[1], key, value[0]))
 
 
 if __name__ == '__main__':
     print('********* Import the csv file ********')
     input_file = input('Import the file: ')
-    print(csv_file(input_file))
+    load_csv_asset(input_file)  # noqa
