d0c06ae19f50fcd0ecc16ae709f50b68e93c3702
Ian Kigen <ian.Gabe.ian@gmail.com>
2018-06-12 16:46:38 +0300

    feat(settings): enable cors (#104)
    * feat(settings): enable cors
    
    * fix(settings): add localhost to cors whitelist
    
    * fix(settings) add cors regex whitelist

--- settings/prod.py
+++ settings/prod.py
@@ -5,3 +5,10 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'art-backend.herokuapp.com'
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'art-dashboard-staging.herokuapp.com'
+)
+
+CORS_ORIGIN_REGEX_WHITELIST = \
+    (r'^(https?:\/\/)?(\w+\.)?((localhost)|(127\.0\.0\.1)):\d{4}', )
