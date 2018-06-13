3113780dd433671318ca5592842acb59d7aef8e1
japhethca <chidiebere.anyigor@gmail.com>
2018-06-13 07:02:08 +0100

    feat(url-import): implements file import from url
    - adds is_valid_url function for validating url
    - adds get_csv_from_url function downloading remote csv files

--- utils/seed_asset.py
+++ utils/seed_asset.py
@@ -7,7 +7,10 @@ project_dir = os.path.dirname(
     os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(project_dir)
 
-from utils.helpers import is_valid_file  # noqa
+from utils.helpers import (
+    is_valid_file, get_csv_from_url, is_valid_url
+    )  # noqa
+
 from utils.asset.post_scripts import (
     post_asset_make, post_asset_category,
     post_asset_model_no, post_asset, post_asset_types,
@@ -19,18 +22,28 @@ django.setup()
 
 
 if __name__ == '__main__':
-    filename = input('Enter csv filename (without .csv): ').strip()
+    filename = input('Enter csv filename '
+                     '(without .csv or url link to csv): ').strip()
     filepath = os.path.abspath(os.path.join(os.path.dirname(__file__))) + '/'  # noqa
 
-    if is_valid_file(filename):
-        with open(filepath + filename + ".csv", 'r', ) as f:
-            file_length = len(f.readlines()) - 1
-            f.seek(0)
+    if is_valid_url(filename):
+        csvfile = get_csv_from_url(filename, filepath)
+        filename = csvfile if csvfile else sys.exit()
 
-            # seed scripts
-            post_asset_category(f, file_length)
-            post_asset_subcategory(f, file_length)
-            post_asset_types(f, file_length)
-            post_asset_make(f, file_length)
-            post_asset_model_no(f, file_length)
-            post_asset(f, file_length)
+    elif is_valid_file(filename):
+        filename += ".csv"
+
+    else:
+        sys.exit()
+
+    with open(filepath + filename, 'r', ) as f:
+        file_length = len(f.readlines()) - 1
+        f.seek(0)
+
+        # seed scripts
+        post_asset_category(f, file_length)
+        post_asset_subcategory(f, file_length)
+        post_asset_types(f, file_length)
+        post_asset_make(f, file_length)
+        post_asset_model_no(f, file_length)
+        post_asset(f, file_length)
