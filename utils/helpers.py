0c1d25716d5f776319ee64ac0defcaa13445c391
Sokunbi Taiwo <sokunbitaiwo82@gmail.com>
2018-06-25 20:21:07 +0100

    chore(import script)
    - create file for skipped records
    
    [Finishes #158254416]

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,9 +1,14 @@
 import os
 import urllib3
 import urllib.parse
+import sys
 
 urllib3.disable_warnings()
 http = urllib3.PoolManager()
+
+
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_dir)
 
 
 def is_valid_file(file_name):
@@ -36,12 +41,15 @@ def display_inserted(result, name=None):
 
 
 def display_skipped(result):
+    file_path = sys.path[-1]
+    record_skipped(result, file_path)
     print('----------------------------------------------------------------\n')
     print('There are {0}  skipped records \n'.format(len(result)))
     print('===============================================================\n')
     if len(result) <= 0:
         print('No record was skipped \n')
     else:
+
         print('{0} \t{1} \t {2}'.format('Line No.', 'Description',
                                         'Error Message'))
         print('------------------------------------------------------------\n')
@@ -89,3 +97,33 @@ def get_csv_from_url(url, filepath):
 
     print('The url does not point to a valid a csv file')
     return None
+
+
+def record_skipped(record, file_path):
+    """
+    Get csv from URL
+    :param record: record  that was skipped
+    :param file_path: path to the output file
+    :return: None
+    """
+    filename = "/skipped.txt"
+    f = open(file_path + filename, 'a+')
+    f.write('======================Skipped records====================')
+    f.write('\n')
+    f.write('Description')
+    f.write('\t' * 3)
+    f.write('Error Message')
+    f.write('\t' * 2)
+    f.write('Line No.')
+    f.write('\n')
+    f.write('************************************************************')
+    f.write('\n')
+    for k, v in record.items():
+        f.write(k)
+        f.write('\t' * 3)
+        for i in range(len(v)):
+            f.write(str(v[i]))
+            f.write('\t')
+        f.write('\n')
+    f.write('\n' * 3)
+    f.close()
