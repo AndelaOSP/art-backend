17a91e8bb7d59534d8d6d31fcd756a51e40e3109
cjmash <jamescollins.jcparty@gmail.com>
2018-06-28 17:11:00 +0300

    ch(deployment): implement ci/cd - implement automatic deployment
    [delivers #156802464]

---
+++ scripts/deploy.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+DEPLOY_SCRIPT_PATH="${HOME}/deploy"
+
+curl -o $DEPLOY_SCRIPT_PATH https://raw.githubusercontent.com/AndelaOSP/bash-helper-modules/master/k8s/deploy
+
+source $DEPLOY_SCRIPT_PATH
+
+DOCKER_REGISTRY=gcr.io
+GCLOUD_SERVICE_KEY_NAME=gcloud-service-key.json
+ALLOWED_DEPLOY_ENVIRONMENTS=('staging', 'production')
+
+require 'PRODUCTION_GOOGLE_COMPUTE_ZONE' $PRODUCTION_GOOGLE_COMPUTE_ZONE
+require 'STAGING_GOOGLE_COMPUTE_ZONE' $STAGING_GOOGLE_COMPUTE_ZONE
+require 'STAGING_CLUSTER_NAME' $STAGING_CLUSTER_NAME
+require 'PRODUCTION_CLUSTER_NAME' $PRODUCTION_CLUSTER_NAME
+require 'PROJECT_NAME' $PROJECT_NAME
+require 'GOOGLE_PROJECT_ID' $GOOGLE_PROJECT_ID
+require 'DOCKER_REGISTRY' $DOCKER_REGISTRY
+require 'GCLOUD_SERVICE_KEY' $GCLOUD_SERVICE_KEY
+# get_hosts(){
+#     CURRENTIPS="$(gcloud compute instances list --project bench-projects | grep  gke-bench-staging-default-pool | awk -v ORS=, '{if ($4) print $4}' | sed 's/,$//')"
+#      echo ${CURRENTIPS}
+# }
+BRANCH_NAME=$CIRCLE_BRANCH
+# set the deployment environment
+setEnvironment $BRANCH_NAME
+# ensure its an allowed deployment environment
+isAllowedDeployEnvironment $ENVIRONMENT
+# get K8s deployment name
+
+getDeploymentName DEPLOYMENT_NAME
+
+# Set image image tag and name
+IMAGE_TAG=$(getImageTag $(getCommitHash))
+IMAGE_NAME=$(getImageName)
+
+main() {
+    installGoogleCloudSdk
+    authWithServiceAccount
+    configureGoogleCloudSdk
+    loginToContainerRegistry _json_key
+    buildAndTagDockerImage .
+    publishDockerImage
+    logoutContainerRegistry $DOCKER_REGISTRY
+    deployToKubernetesCluster backend
+}
+
+main
--- settings/prod.py
+++ settings/prod.py
@@ -1,10 +1,9 @@
 from .base import *  # noqa: F403,F401
-
+import os
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'art-backend.herokuapp.com'
-]
+ALLOWED_HOSTS = [os.environ.get('MY_HOST_IP'), 'api-staging-art.andela.com',
+                 'art-backend.herokuapp.com']
 
 CORS_ORIGIN_WHITELIST = (
     'art-dashboard-staging.herokuapp.com'
