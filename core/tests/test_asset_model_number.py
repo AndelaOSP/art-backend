01edcc2627f7dc47e3dac95c781f160e41e35db8
nelmandela <nelsonmandela784@gmail.com>
2018-07-04 17:03:10 +0300

    bug[validation]: Unique validation   -Enable  validation for unique values on asset  category,asset subcategory,asset type,asset make and asset model number   -Add tests

--- core/tests/test_asset_model_number.py
+++ core/tests/test_asset_model_number.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db.models.deletion import ProtectedError
 
 from ..models import AssetModelNumber, AssetMake, AssetType, \
@@ -37,6 +38,15 @@ class AssetModelNumberTestCase(CoreBaseTestCase):
         AssetModelNumber.objects.create(model_number="101505401",
                                         make_label=self.asset_make)
         self.assertEqual(AssetModelNumber.objects.count(), 2)
+
+    def test_cannot_add_existing_asset_make(self):
+        self.assertEqual(AssetMake.objects.count(), 1)
+        with self.assertRaises(ValidationError):
+            AssetModelNumber.objects.create(
+                model_number="101505400",
+                make_label=self.asset_make
+            )
+        self.assertEqual(AssetMake.objects.count(), 1)
 
     def test_can_edit_model_number(self):
         self.laptop_model_number.model_number = "101505400"
