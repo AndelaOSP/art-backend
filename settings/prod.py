6ac834d18d2853d890defc3af0dc55f679604826
cjmash <jamescollins.jcparty@gmail.com>
2018-07-10 17:55:14 +0300

    ch(cors): whitelist cors origin for staging
    [delivers #158946377]

--- settings/prod.py
+++ settings/prod.py
@@ -2,12 +2,7 @@ from .base import *  # noqa: F403,F401
 import os
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('MY_HOST_IP'), 'api-staging-art.andela.com',
-                 'art-backend.herokuapp.com']
-
-CORS_ORIGIN_WHITELIST = (
-    'art-dashboard-staging.herokuapp.com'
-)
+ALLOWED_HOSTS = [os.environ.get('MY_HOST_IP')]
 
 CORS_ORIGIN_REGEX_WHITELIST = \
-    (r'^(https?:\/\/)?(\w+\.)?((localhost)|(127\.0\.0\.1)):\d{4}', )
+    (r'^(https?:\/\/)?(.+\.)?((andela\.com))', )
