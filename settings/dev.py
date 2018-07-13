cbedeb28bd145039ca36e26103c70f15100d5e1e
Collins Macharia <jamescollins.jcparty@gmail.com>
2018-07-13 13:12:57 +0300

    ch(cors): whitelist cors origin for staging (#126)
    [delivers #158946377]

--- settings/dev.py
+++ settings/dev.py
@@ -10,7 +10,17 @@ MIDDLEWARE += [  # noqa ignore=F405
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
-ALLOWED_HOSTS = [os.environ.get('MY_HOST_IP'), 'api-staging-art.andela.com']
+
+ALLOWED_HOSTS = [os.environ.get('MY_HOST_IP'), 'api-staging-art.andela.com',
+                 '127.0.0.1']
+
+CORS_ORIGIN_WHITELIST = (
+    'art-dashboard-staging.herokuapp.com'
+)
+
+CORS_ORIGIN_REGEX_WHITELIST = (
+    r'^(https?:\/\/)?(.+\.)?(andela\.com)|((localhost)|(127\.0\.0\.1):\d{4})',
+)
 
 INTERNAL_IPS = [
     '0.0.0.0',
