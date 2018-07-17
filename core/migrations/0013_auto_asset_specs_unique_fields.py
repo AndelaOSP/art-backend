ff506ce6e6b749359cb5f1e076250f61c0814062
japhethca <chidiebere.anyigor@gmail.com>
2018-07-17 09:56:54 +0100

    feat(incidence-report): fix failing tests

--- core/migrations/0013_auto_asset_specs_unique_fields.py
+++ core/migrations/0013_auto_asset_specs_unique_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0011_asset_purchase_date'),
+        ('core', '0012_assetincidentreport_submitted_by'),
     ]
 
     operations = [
