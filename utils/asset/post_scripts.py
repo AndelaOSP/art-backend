523f142344da6b63b9d3106cf0c636d57bbcb958
egichuri <eric.gichuri@andela.com>
2018-07-28 15:41:11 +0300

    more  refactor
    Co-authored-by: Collin Mutembei <collin.mutembei@andela.com>

--- utils/asset/post_scripts.py
+++ utils/asset/post_scripts.py
@@ -16,27 +16,32 @@ skipped_rows = []
 
 
 def write_skipped_records(records):
-    """
-    Write skipped record to a file
-    :param record: record  that was skipped
-    :param file_path: path to the output file
-    :return: None
-    """
     filename = "/skipped.csv"
     fieldnames = ('Row', 'Make', 'Type', 'Asset Code', 'Category',
                   'Sub-Category', 'Model Number', 'Serial No.', 'Error')
     with open(file_path + filename, "w") as csv_file:
-        print("Writing skipped rows to {}".format(filename))
+        print("Writing {} skipped rows to {}".format(len(records), filename))
         dw = csv.DictWriter(csv_file, delimiter=',', fieldnames=fieldnames)
         dw.writeheader()
         for row in records:
             dw.writerow(row)
 
 
-def record_errors(row, count, object_error):
+def record_errors(row, row_count, object_error):
+    for line in skipped_rows:
+        if line.get('Row') == row_count:
+            line['Error'] += object_error
+            return
     row['Error'] = object_error
-    row['Row'] = count
+    row['Row'] = row_count
     skipped_rows.append(row)
+
+
+def create_object(collection, parent=None, row=None, row_count=None, **values):
+    obj, success = collection_bootstrap(collection, parent, **values)
+    if success:
+        return obj
+    record_errors(row, row_count, obj)
 
 
 def post_data(file):
@@ -47,72 +52,45 @@ def post_data(file):
         row_count = 0
         for row in data:
             row_count += 1
+            row_data = {'row': row, 'row_count': row_count}
             progress.write('Processing row {} of {}'.format(
                 row_count, file_length))
             progress.update()
             category_value = row.get('Category').strip()
+            category = create_object(
+                'AssetCategory', category_name=category_value,
+                **row_data)
+
             subcategory_value = row.get('Sub-Category').strip()
+            subcategory = create_object(
+                'AssetSubCategory',
+                parent={'asset_category': category},
+                sub_category_name=subcategory_value, **row_data)
+
             type_value = row.get('Type').strip()
+            asset_type = create_object(
+                'AssetType', parent={'asset_sub_category': subcategory},
+                asset_type=type_value, **row_data)
+
             make_value = row.get('Make').strip()
+            asset_make = create_object(
+                'AssetMake', parent={'asset_type': asset_type},
+                make_label=make_value, **row_data)
+
             modelnumber_value = row.get('Model Number').strip()
-            assetcode_value = row.get('Asset Code').strip()
-            serialnumber_value = row.get('Serial No.').strip()
-            category_object, success = collection_bootstrap(
-                'AssetCategory', category_name=category_value)
-            if success:
-                subcategory_fields = {'sub_category_name': subcategory_value}
-                subcategory_object, success = collection_bootstrap(
-                    'AssetSubCategory',
-                    parent={'asset_category': category_object},
-                    **subcategory_fields)
-            else:
-                record_errors(row, row_count, category_object)
-                continue
+            asset_model_no = create_object(
+                'AssetModelNumber', parent={'make_label': asset_make},
+                model_number=modelnumber_value, **row_data)
 
-            if success:
-                type_fields = {'asset_type': type_value}
-                type_object, success = collection_bootstrap(
-                    'AssetType',
-                    parent={'asset_sub_category': subcategory_object},
-                    **type_fields)
-            else:
-                record_errors(row, row_count, subcategory_object)
-                continue
+            assetcode_value = row.get('Asset Code').strip() or None
+            serialnumber_value = row.get('Serial No.').strip() or None
 
-            if success:
-                make_fields = {'make_label': make_value}
-                make_object, success = collection_bootstrap(
-                    'AssetMake',
-                    parent={'asset_type': type_object},
-                    **make_fields)
-            else:
-                record_errors(row, row_count, type_object)
-                continue
-
-            if success:
-                modelnumber_fields = {'model_number': modelnumber_value}
-                modelnumber_object, success = collection_bootstrap(
-                    'AssetModelNumber',
-                    parent={'make_label': make_object},
-                    **modelnumber_fields)
-            else:
-                record_errors(row, row_count, make_object)
-                continue
-
-            if success:
-                asset_fields = {
-                    'asset_code': assetcode_value,
-                    'serial_number': serialnumber_value
-                }
-                asset_object, success = collection_bootstrap(
-                    'Asset',
-                    parent={'model_number': modelnumber_object},
-                    **asset_fields)
-            else:
-                record_errors(row, row_count, modelnumber_object)
-                continue
-
-            if not success:
-                record_errors(row, row_count, asset_object)
+            asset_fields = {
+                'asset_code': assetcode_value,
+                'serial_number': serialnumber_value
+            }
+            asset_model_no = create_object(
+                'Asset', parent={'model_number': asset_model_no},
+                **row_data, **asset_fields)
 
     write_skipped_records(skipped_rows)
--- utils/seed_bootstrap.py
+++ utils/seed_bootstrap.py
@@ -3,17 +3,18 @@ from core.models.asset import Asset
 
 
 def collection_bootstrap(collection, parent=None, **fields):
+    if parent is not None:
+        missing_parent = [a for a, b in parent.items() if not b]
+        if len(missing_parent) > 0:
+            return missing_parent, False
     collection = apps.get_model('core', collection)
     missing_fields = [a for a, b in fields.items() if not b]
 
-    for field in missing_fields:
-        fields[field] = None
-
     if missing_fields and collection is not Asset:
-        return 'Missing fields: {}'.format(missing_fields), False
+        return missing_fields, False
 
     if collection is Asset and len(missing_fields) > 1:
-        return 'Missing fields: {}'.format(missing_fields), False
+        return missing_fields, False
     return load_to_db(collection, parent, **fields)
 
 
@@ -21,7 +22,7 @@ def load_to_db(collection, parent=None, **fields):
     try:
         obj = collection.objects.get(**fields)
         if collection is Asset:
-            return 'Asset already imported', False
+            return ['Asset already imported'], False
         return obj, True
     except Exception:
         if parent:
