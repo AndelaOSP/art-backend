16d13fdfe5a373a61bc93499293e31f6b68421c9
egichuri <eric.gichuri@andela.com>
2018-08-24 23:02:39 +0300

    use commit messages from dev

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,19 +2,25 @@
 
 # current Git branch
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+devBranch=develop
 
 # v1.0.0, v1.5.2, etc.
 lastTag=$(git describe --tags $(git rev-list --tags --max-count=1))
 
+git reset --hard origin/$devBranch
+git pull origin $devBranch
 if [ -z "$lastTag" ] ; then
+    tagMessage=$(git log $devBranch --no-merges --pretty=format:"%s")
     lastTag=v0.9.9
+else
+    tagMessage=$(git log $lastTag..$devBranch --no-merges --pretty=format:"%s")
 fi
 
+# Increment tag number
 removedV=${lastTag//v}
 part1=$(cut -d'.' -f1 <<<"$removedV")
 part2=$(cut -d'.' -f2 <<<"$removedV")
 part3=$(cut -d'.' -f3 <<<"$removedV")
-
 part3=$((part3 + 1))
 
 if (( $part3 > 9 )) ; then
@@ -30,9 +36,10 @@ versionLabel=v$part1.$part2.$part3
 
 echo "New version: $versionLabel"
 
-# create tag for new version from -master
-tagMessage=$(git log -1 --pretty=%B)
 git tag -a $versionLabel -m "$tagMessage"
 
-# push including all tags
+echo "Release Notes:"
+echo $tagMessage
+
+# push including created tag
 git push origin $versionLabel
