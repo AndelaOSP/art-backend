5512407dbf7447c5daa1ecb8d81396dd357123cc
JoanNgatia <joan.ngatia@andela.com>
2018-10-15 17:41:35 +0300

    Update tests to reflect new location on office block

--- core/models/officeblock.py
+++ core/models/officeblock.py
@@ -4,9 +4,9 @@ from rest_framework.exceptions import ValidationError
 
 class OfficeBlock(models.Model):
     name = models.CharField(max_length=50,
-                            blank=False, null=False, unique=True)
+                            blank=False, null=False)
     location = models.ForeignKey(
-        'AndelaCentre', on_delete=models.PROTECT, null=True, editable=True)
+        'AndelaCentre', on_delete=models.PROTECT, null=True)
 
     def clean(self):
         self.name = " ".join(self.name.title().split())
