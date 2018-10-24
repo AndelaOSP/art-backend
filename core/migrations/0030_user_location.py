930c0fb2ea750187bb035f298cd85afb147f6443
andela-pijege <precious.ijege@andela.com>
2018-10-24 13:46:52 +0100

    fix conflicting migrations

--- core/migrations/0030_user_location.py
+++ core/migrations/0030_user_location.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0001_initial'),
+        ('core', 'populate_officeblock_location'),
     ]
 
     operations = [
