8ec01b75f153962ba784ae27efbda52f26884eb9
esir <esirkings@gmail.com>
2018-11-21 11:00:25 +0300

    Update ART user if AIS data have changed

--- scripts/sync_users.py
+++ scripts/sync_users.py
@@ -9,7 +9,7 @@ from django.contrib.auth import get_user_model
 
 url = "https://api-prod.andela.com/api/v1/users"
 
-headers = {'Authorization': os.getenv('TOKEN')}
+headers = {'Authorization': os.getenv('Bearer')}
 
 response = requests.get(url, headers=headers)
 
@@ -20,18 +20,37 @@ sys.path.append(below_path)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 django.setup()
 
-from core.models import AndelaCentre  # NOQA
+from core.models import AndelaCentre, User  # NOQA
 
 user = get_user_model()
 
 vlst = list(response.json().values())
 
-for value in vlst[0]: # NOQA
+for value in vlst[0]:  # NOQA
 
     values = dict()
     values.update({'email': value.get('email')})
 
-    if user.objects.filter(email=values.get('email')).exists():
+    cohort = value.get('cohort')
+    cohort_num = None
+    if cohort:
+        if cohort.get('name').lower() == 'staff':
+            cohort_num = 0
+        else:
+            cohort_num = int(re.search(r'\d+', cohort.get('name')).group())
+
+    user, created = User.objects.get_or_create(email=values.get('email'))
+
+    if not created:
+        # check if picture, cohort or slack_handle have changed
+        if user.picture != value.get('picture'):
+            user.picture = value.get('picture')
+        if user.cohort != cohort_num:
+            user.cohort = cohort_num
+        if user.slack_handle != value.get('slack'):
+            user.slack_handle = value.get('slack')
+        user.save()
+
         continue
 
     if value.get('location'):
@@ -39,17 +58,9 @@ for value in vlst[0]: # NOQA
 
         if AndelaCentre.objects.filter(centre_name=location).exists():
             andelaCenter = AndelaCentre.objects.get(centre_name=location)
-        else:
-            andelaCenter = AndelaCentre.objects.create(centre_name=location)
+            value['location'] = andelaCenter
 
-        value['location'] = andelaCenter
-
-    cohort = value.get('cohort')
     if cohort:
-        if cohort.get('name').lower() == 'staff':
-            cohort_num = 0
-        else:
-            cohort_num = int(re.search(r'\d+', cohort.get('name')).group())
         values.update({'cohort': cohort_num})
 
     values.update({'slack_handle': value.get('slack')})
