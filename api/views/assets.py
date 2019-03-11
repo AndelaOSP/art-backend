6d261cd1516614a2049b5c25626a2fc9061217bd
andela-pijege <precious.ijege@andela.com>
2019-03-11 12:05:13 +0100

    fix errors

--- api/views/assets.py
+++ api/views/assets.py
@@ -4,9 +4,9 @@ import csv
 import logging
 import os
 from itertools import chain
-import xlsxwriter
 
 # Third-Party Imports
+import xlsxwriter
 from django.conf import settings
 from django.core.validators import ValidationError
 from django.http import FileResponse
@@ -369,13 +369,13 @@ class AssetsImportViewSet(APIView):
             return Response(
                 {"error": "Csv file to import from not provided"}, status=400
             )
-        if not file_object.name.endswith('.csv'):
+        if not file_object.name.endswith(".csv"):
             return Response(
                 {"error": "File type not surported, import a CSV file"}, status=400
             )
-        file_obj = codecs.iterdecode(file_object, 'utf-8')
+        file_obj = codecs.iterdecode(file_object, "utf-8")
         csv_reader = csv.DictReader(file_obj, delimiter=",")
-        if not (csv_reader.fieldnames and ' '.join(csv_reader.fieldnames).strip()):
+        if not (csv_reader.fieldnames and " ".join(csv_reader.fieldnames).strip()):
             return Response({"error": "CSV file is empty"}, status=400)
         csv_values = []
         for line in csv_reader.reader:
@@ -387,7 +387,7 @@ class AssetsImportViewSet(APIView):
         user = self.request.user
         response = {}
         error = False
-        file_obj = codecs.iterdecode(file_object, 'utf-8')
+        file_obj = codecs.iterdecode(file_object, "utf-8")
         csv_reader = csv.DictReader(file_obj, delimiter=",")
         if not process_file(csv_reader, user=user):
             path = request.build_absolute_uri(reverse("skipped"))
@@ -435,7 +435,7 @@ class SampleImportFile(APIView):
 
         file = open(filename, "rb")
         response = FileResponse(
-            file, content_type="text/csv", filename='sample_import_file.csv'
+            file, content_type="text/csv", filename="sample_import_file.csv"
         )
         response[
             "Content-Disposition"
@@ -449,48 +449,60 @@ class ExportAssetsDetails(APIView):
     authentication_classes = (FirebaseTokenAuthentication,)
 
     def get(self, request):
-        assets = models.Asset.objects.filter(asset_location__name=request.user.location.name)
+        assets = models.Asset.objects.filter(
+            asset_location__name=request.user.location.name
+        )
         serializer = AssetSerializer(assets, many=True)
         if len(serializer.data) == 0:
             return Response({"error": "You have no assets"}, status=400)
         self.create_sheet(serializer.data)
-        return Response({
-            "Success": "Assets details have been exported successfully, Find file in root folder"}, status=200)
+        return Response(
+            {
+                "Success": "Assets details have been exported successfully, Find file in root folder"
+            },
+            status=200,
+        )
 
     def create_sheet(self, assets_list):
         asset_types = []
         for asset in assets_list:
-            if asset.get('asset_type') not in asset_types:
-                asset_types.append(asset.get('asset_type'))
+            if asset.get("asset_type") not in asset_types:
+                asset_types.append(asset.get("asset_type"))
 
-        workbook = xlsxwriter.Workbook('assets.xlsx')
-        bold = workbook.add_format({'bold': True, 'bg_color': 'silver'})
+        workbook = xlsxwriter.Workbook("assets.xlsx")
+        bold = workbook.add_format({"bold": True, "bg_color": "silver"})
         for asset_type in asset_types:
             worksheet = workbook.add_worksheet(asset_type)
-            worksheet.write('A1', 'Make', bold)
-            worksheet.write('B1', 'Location', bold)
-            worksheet.write('C1', 'Asset Code', bold)
-            worksheet.write('D1', 'Serial No', bold)
-            worksheet.write('E1', 'Model No', bold)
-            worksheet.write('F1', 'Assigned To', bold)
-            worksheet.write('G1', 'Status', bold)
-            worksheet.write('H1', 'Verified', bold)
-            worksheet.write('I1', 'Notes', bold)
+            worksheet.write("A1", "Make", bold)
+            worksheet.write("B1", "Location", bold)
+            worksheet.write("C1", "Asset Code", bold)
+            worksheet.write("D1", "Serial No", bold)
+            worksheet.write("E1", "Model No", bold)
+            worksheet.write("F1", "Assigned To", bold)
+            worksheet.write("G1", "Status", bold)
+            worksheet.write("H1", "Verified", bold)
+            worksheet.write("I1", "Notes", bold)
             grouped_assets = []
             for asset in assets_list:
-                if asset.get('asset_type') == asset_type:
+                if asset.get("asset_type") == asset_type:
                     grouped_assets.append(asset)
             row = 1
             column = 0
             for asset in grouped_assets:
-                worksheet.write(row, column, asset.get('asset_make', ''))
-                worksheet.write(row, column + 1, asset.get('asset_location', ''))
-                worksheet.write(row, column + 2, asset.get('asset_code'))
-                worksheet.write(row, column + 3, asset.get('serial_number', ''))
-                worksheet.write(row, column + 4, asset.get('model_number', ''))
-                worksheet.write(row, column + 5, asset.get('assigned_to')['email'] if asset.get('assigned_to') else '')
-                worksheet.write(row, column + 6, asset.get('current_status', ''))
-                worksheet.write_boolean(row, column + 7, asset.get('verified', ''))
-                worksheet.write(row, column + 8, asset.get('notes', ''))
+                worksheet.write(row, column, asset.get("asset_make", ""))
+                worksheet.write(row, column + 1, asset.get("asset_location", ""))
+                worksheet.write(row, column + 2, asset.get("asset_code"))
+                worksheet.write(row, column + 3, asset.get("serial_number", ""))
+                worksheet.write(row, column + 4, asset.get("model_number", ""))
+                worksheet.write(
+                    row,
+                    column + 5,
+                    asset.get("assigned_to")["email"]
+                    if asset.get("assigned_to")
+                    else "",
+                )
+                worksheet.write(row, column + 6, asset.get("current_status", ""))
+                worksheet.write_boolean(row, column + 7, asset.get("verified", ""))
+                worksheet.write(row, column + 8, asset.get("notes", ""))
                 row += 1
         workbook.close()
