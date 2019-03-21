6547d7ef941f58c2764264f3f7f2c46f09b9c826
egichuri <eric.gichuri@andela.com>
2019-03-21 15:01:48 +0300

    refactor slack incidence report

--- core/slack_bot.py
+++ core/slack_bot.py
@@ -5,9 +5,10 @@ import os
 
 # Third-Party Imports
 from django.contrib.auth import get_user_model
-from rest_framework import status
-from rest_framework.response import Response
 from slackclient import SlackClient
+
+# App Imports
+from core.constants import DAMAGE, LOSS
 
 logger = logging.getLogger(__name__)
 
@@ -19,7 +20,6 @@ class SlackIntegration(object):
         """set the slack token"""
         slack_token = os.getenv('SLACK_TOKEN')
         self.incidence_ts = {}
-        self.user_email = ''
         if slack_token:
             self.slack_client = SlackClient(slack_token)
 
@@ -125,148 +125,117 @@ class SlackIntegration(object):
         from core.models import Asset, AssetIncidentReport
 
         User = get_user_model()
-        if data.get('payload') is None:
+        payload = data.get('payload', None)
+        if payload:
+            payload = json.loads(payload)
+            channel_id = payload.get('channel').get('id')
+            user_id = payload.get('user').get('id')
+        else:
             channel_id = data.get('channel_id')
             user_id = data.get('user_id')
-            self.slack_client.api_call(
-                "chat.postEphemeral",
-                username='Art-incidence-report',
-                channel=channel_id,
-                user=user_id,
-                response_type='ephemeral',
-                text="Would you like to log incident report?",
-                attachments=[
-                    {
-                        "fallback": "You are unable to choose a game",
-                        "callback_id": "choice_made",
-                        "color": "#3AA3E3",
-                        "attachment_type": "default",
-                        "actions": [
-                            {
-                                "name": "game",
-                                "text": "Yes",
-                                "type": "button",
-                                "value": "yes",
-                            },
-                            {
-                                "name": "game",
-                                "text": "No",
-                                "type": "button",
-                                "value": "no",
-                            },
-                        ],
-                    }
-                ],
-            )
-            return Response(status=status.HTTP_200_OK)
+        user_email = self.get_user_slack_email(user_id)
 
-        payload = data.get('payload', None)
-        payload = json.loads(payload)
+        if not payload:
+            assets = Asset.objects.filter(assigned_to__user__email=user_email)
+            if assets.count() == 0:
+                no_asset_resp = self.slack_client.api_call(
+                    'chat.postEphemeral',
+                    username='Art-incidence-report',
+                    channel=channel_id,
+                    user=user_id,
+                    response_url=data.get('response_url'),
+                    response_type='ephemeral',
+                    text='Sorry, No Asset is assigned to you!!!',
+                )
+                if not no_asset_resp.get('ok'):
+                    error = no_asset_resp.get('error')
+                    logger.error(f'Error sending message for {user_id}: {error}')
+                    return False
+                return True
 
-        if payload['type'] == 'dialog_cancellation':
-            return Response(status=status.HTTP_200_OK)
-
-        if payload['type'] == 'interactive_message':
-            self.incidence_ts['message_ts'] = payload['message_ts']
-            if payload['callback_id'] == 'choice_made':
-                if payload['actions'][0]['value'] == 'yes':
-                    self.user_email = self.get_user_slack_email(payload['user']['id'])
-                    assets = Asset.objects.filter(
-                        assigned_to__user__email=self.user_email
-                    )
-                    if len(assets) == 0:
-                        no_asset = self.slack_client.api_call(
-                            'chat.postEphemeral',
-                            username='Art-incidence-report',
-                            channel=payload['channel']['id'],
-                            user=payload['user']['id'],
-                            response_url=payload['response_url'],
-                            response_type='ephemeral',
-                            text='Sorry, No Asset is assigned to you!!!',
-                        )
-                        if no_asset:
-                            return Response(status=status.HTTP_200_OK)
-
-                    self.slack_client.api_call(
-                        'dialog.open',
-                        trigger_id=payload['trigger_id'],
-                        dialog={
-                            "title": "Andela Resources Tracker",
-                            "submit_label": "Submit",
-                            "notify_on_cancel": True,
-                            "callback_id": "{}_incidence_report_1".format(
-                                payload['user']['id']
-                            ),
-                            "elements": [
-                                {
-                                    'label': 'Asset',
-                                    'type': 'select',
-                                    'name': 'asset',
-                                    'options': [
-                                        {
-                                            'label': '{}, {}, {}'.format(
-                                                asset.asset_code,
-                                                asset.serial_number,
-                                                asset.model_number,
-                                            ),
-                                            'value': asset.id,
-                                        }
-                                        for asset in assets
-                                    ],
-                                },
-                                {
-                                    'label': 'Incident type',
-                                    'type': 'select',
-                                    'name': 'incident_type',
-                                    'options': [
-                                        {'label': 'Loss', 'value': 'Loss'},
-                                        {'label': 'Damage', 'value': 'Damage'},
-                                    ],
-                                },
-                                {
-                                    'label': 'Incident location',
-                                    'type': 'text',
-                                    'name': 'incident_location',
-                                    'hint': '30 second summary of the problem',
-                                },
-                                {
-                                    'label': 'Incident description',
-                                    'type': 'textarea',
-                                    'name': 'incident_description',
-                                    'hint': '30 second description of the problem',
-                                },
-                                {
-                                    'label': 'Police Abstract Obtained',
-                                    'type': 'textarea',
-                                    'name': 'police_abstract_obtained',
-                                    'hint': 'Summary of Police Report',
-                                },
+            dialog_response = self.slack_client.api_call(
+                'dialog.open',
+                trigger_id=data.get('trigger_id'),
+                dialog={
+                    "title": "Andela Resources Tracker",
+                    "submit_label": "Submit",
+                    "notify_on_cancel": True,
+                    "callback_id": "{}_incidence_report_1".format(user_id),
+                    "elements": [
+                        {
+                            'label': 'Asset',
+                            'type': 'select',
+                            'name': 'asset',
+                            'options': [
+                                {'label': '{}'.format(str(asset)), 'value': asset.id}
+                                for asset in assets
                             ],
                         },
-                    )
-                    return Response(status=status.HTTP_200_OK)
-                return Response(status=status.HTTP_200_OK)
+                        {
+                            'label': 'Incident type',
+                            'type': 'select',
+                            'name': 'incident_type',
+                            'options': [
+                                {'label': LOSS, 'value': LOSS},
+                                {'label': DAMAGE, 'value': DAMAGE},
+                            ],
+                        },
+                        {
+                            'label': 'Incident location',
+                            'type': 'text',
+                            'name': 'incident_location',
+                            'hint': '30 second summary of the problem',
+                        },
+                        {
+                            'label': 'Incident description',
+                            'type': 'textarea',
+                            'name': 'incident_description',
+                            'hint': '30 second description of the problem',
+                        },
+                        {
+                            'label': 'Police Abstract Obtained',
+                            'type': 'textarea',
+                            'name': 'police_abstract_obtained',
+                            'hint': 'Summary of Police Report',
+                        },
+                    ],
+                },
+            )
+            if not dialog_response.get('ok'):
+                error = dialog_response.get('error')
+                logger.error(f'Error sending message for {user_id}: {error}')
+                return False
+            return True
 
-        if payload['type'] == 'dialog_submission':
+        if payload.get('type') == 'dialog_cancellation':
+            return True
+
+        if payload.get('type') == 'dialog_submission':
             report = AssetIncidentReport()
-            report.asset = Asset.objects.get(id=int(payload['submission']['asset']))
-            report.incident_type = payload['submission']['incident_type']
-            report.incident_location = payload['submission']['incident_location']
-            report.incident_description = payload['submission']['incident_description']
-            report.police_abstract_obtained = payload['submission'][
+            report.asset = Asset.objects.get(id=int(payload.get('submission')['asset']))
+            report.incident_type = payload.get('submission')['incident_type']
+            report.incident_location = payload.get('submission')['incident_location']
+            report.incident_description = payload.get('submission')[
+                'incident_description'
+            ]
+            report.police_abstract_obtained = payload.get('submission')[
                 'police_abstract_obtained'
             ]
-            report.submitted_by = User.objects.get(email=self.user_email)
-            report.save()
-            if report:
+            report.submitted_by = User.objects.get(email=user_email)
+            try:
+                report.save()
+            except Exception as e:
+                logger.error(str(e))
+                return False
+            else:
                 smile = ":simple_smile::simple_smile::simple_smile:"
                 self.slack_client.api_call(
                     "chat.postEphemeral",
                     username='Art-incidence-report',
-                    channel=payload['channel']['id'],
-                    user=payload['user']['id'],
-                    response_url=payload['response_url'],
+                    channel=channel_id,
+                    user=user_id,
+                    response_url=payload.get('response_url'),
                     response_type='ephemeral',
                     text="Incident report logged. Thank you!!! {}".format(smile),
                 )
-                return Response(status=status.HTTP_200_OK)
+            return True
