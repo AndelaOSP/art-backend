b996aded2d96ea33a780b02131df7d8e2d9ad8fe
Dominic Motuka <dominic.motuka@gmail.com>
2019-03-27 19:52:33 +0300

    Ignore all shellcheck instances

--- scripts/update_apt.sh
+++ scripts/update_apt.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2059
 set -eo pipefail
 
 rm /etc/apt/sources.list
-printf "deb http://archive.debian.org/debian/ jessie-backports main" |  tee -a /etc/apt/sources.list
-printf "deb-src http://archive.debian.org/debian/ jessie-backports main" |  tee -a /etc/apt/sources.list
-printf "Acquire::Check-Valid-Until false;" |  tee -a /etc/apt/apt.conf.d/10-nocheckvalid
-printf 'Package: *\nPin: origin "archive.debian.org"\nPin-Priority: 500' |  tee -a /etc/apt/preferences.d/10-archive-pin
+echo "deb http://archive.debian.org/debian/ jessie-backports main" |  tee -a /etc/apt/sources.list
+echo "deb-src http://archive.debian.org/debian/ jessie-backports main" |  tee -a /etc/apt/sources.list
+echo "Acquire::Check-Valid-Until false;" |  tee -a /etc/apt/apt.conf.d/10-nocheckvalid
+echo 'Package: *\nPin: origin "archive.debian.org"\nPin-Priority: 500' |  tee -a /etc/apt/preferences.d/10-archive-pin
 apt-get update
