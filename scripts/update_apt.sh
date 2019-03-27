526c97e5af8f8e5a6cb7c479cb5f01c56a1e3c3c
Dominic Motuka <dominic.motuka@gmail.com>
2019-03-27 19:40:41 +0300

    Expand escape sequences with 'printf'

--- scripts/update_apt.sh
+++ scripts/update_apt.sh
@@ -2,8 +2,8 @@
 set -eo pipefail
 
 rm /etc/apt/sources.list
-echo "deb http://archive.debian.org/debian/ jessie-backports main" |  tee -a /etc/apt/sources.list
-echo "deb-src http://archive.debian.org/debian/ jessie-backports main" |  tee -a /etc/apt/sources.list
-echo "Acquire::Check-Valid-Until false;" |  tee -a /etc/apt/apt.conf.d/10-nocheckvalid
-echo 'Package: *\nPin: origin "archive.debian.org"\nPin-Priority: 500' |  tee -a /etc/apt/preferences.d/10-archive-pin
+printf "deb http://archive.debian.org/debian/ jessie-backports main" |  tee -a /etc/apt/sources.list
+printf "deb-src http://archive.debian.org/debian/ jessie-backports main" |  tee -a /etc/apt/sources.list
+printf "Acquire::Check-Valid-Until false;" |  tee -a /etc/apt/apt.conf.d/10-nocheckvalid
+printf 'Package: *\nPin: origin "archive.debian.org"\nPin-Priority: 500' |  tee -a /etc/apt/preferences.d/10-archive-pin
 apt-get update
