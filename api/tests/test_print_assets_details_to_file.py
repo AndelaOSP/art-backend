3aa6c17bcf7765b91ffa6669b508da5349937359
egichuri <eric.gichuri@andela.com>
2019-03-28 13:52:43 +0300

    Update Export assets url to accommodate query params

--- api/tests/test_print_assets_details_to_file.py
+++ api/tests/test_print_assets_details_to_file.py
@@ -4,11 +4,12 @@ from unittest.mock import patch
 # Third-Party Imports
 from django.apps import apps
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 from rest_framework.test import APIClient
 
 # App Imports
 from api.tests import APIBaseTestCase
-from core.models import Asset
+from core.models import AllocationHistory, Asset
 
 User = get_user_model()
 client = APIClient()
@@ -35,8 +36,8 @@ class PrintAssetsDetailsTestCase(APIBaseTestCase):
         response = client.get(
             self.print_asset_url, HTTP_AUTHORIZATION="Token {}".format(self.token_admin)
         )
-        Asset.objects.count()
         self.assertEqual(response.status_code, 200)
+        self.assertIn('success', response.json().keys())
 
     @patch("api.authentication.auth.verify_id_token")
     def test_authenticated_admin_cant_print_assets_in_diffrent_location(
@@ -92,3 +93,79 @@ class PrintAssetsDetailsTestCase(APIBaseTestCase):
         )
         self.assertEqual(response.data, {"detail": 'Method "DELETE" not allowed.'})
         self.assertEqual(response.status_code, 405)
+
+    @patch("api.authentication.auth.verify_id_token")
+    def test_authenticated_admin_can_print_assets_with_filters(
+        self, mock_verify_id_token
+    ):
+        Asset.objects.create(
+            asset_code="IC00145328",
+            serial_number="SN50123455S",
+            model_number=self.assetmodel,
+            asset_location=self.centre,
+        )
+        available_assets = Asset.objects.filter(current_status='Available')
+        mock_verify_id_token.return_value = {"email": self.admin_user.email}
+        response = client.get(
+            '{}?current_status=Available'.format(self.print_asset_url),
+            HTTP_AUTHORIZATION="Token {}".format(self.token_admin),
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            '{} assets exported'.format(available_assets.count()),
+            response.data['success'],
+        )
+
+        AllocationHistory.objects.create(
+            asset=available_assets[0], current_owner=self.user.assetassignee
+        )
+        available_assets = Asset.objects.filter(current_status='Available')
+        response = client.get(
+            '{}?current_status=Available'.format(self.print_asset_url),
+            HTTP_AUTHORIZATION="Token {}".format(self.token_admin),
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            '{} assets exported'.format(available_assets.count()),
+            response.data['success'],
+        )
+
+    @patch("api.authentication.auth.verify_id_token")
+    def test_authenticated_admin_can_print_assets_with_multiple_filters(
+        self, mock_verify_id_token
+    ):
+        Asset.objects.create(
+            asset_code="IC00145328",
+            serial_number="SN50123455S",
+            model_number=self.assetmodel,
+            asset_location=self.centre,
+        )
+        available_assets = Asset.objects.filter(
+            Q(current_status='Available') | Q(current_status='Allocated')
+        )
+        mock_verify_id_token.return_value = {"email": self.admin_user.email}
+        AllocationHistory.objects.create(
+            asset=available_assets[0], current_owner=self.user.assetassignee
+        )
+        response = client.get(
+            '{}?current_status=Available&current_status=Allocated'.format(
+                self.print_asset_url
+            ),
+            HTTP_AUTHORIZATION="Token {}".format(self.token_admin),
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            '{} assets exported'.format(available_assets.count()),
+            response.data['success'],
+        )
+
+    @patch("api.authentication.auth.verify_id_token")
+    def test_authenticated_admin_can_print_assets_with_invalid_filters(
+        self, mock_verify_id_token
+    ):
+        mock_verify_id_token.return_value = {"email": self.admin_user.email}
+        response = client.get(
+            '{}?test=test'.format(self.print_asset_url),
+            HTTP_AUTHORIZATION="Token {}".format(self.token_admin),
+        )
+        self.assertEqual(response.status_code, 400)
