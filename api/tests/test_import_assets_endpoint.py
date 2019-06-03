cb3ab3cebf566380ab9ca93b448b1da79f0c2dc6
graycadeau <g.zawadi.w.gmail.com>
2019-06-03 20:57:07 +0300

    - test

--- api/tests/test_import_assets_endpoint.py
+++ api/tests/test_import_assets_endpoint.py
@@ -158,3 +158,32 @@ class AssetsUploadTestCase(APIBaseTestCase):
             )
         self.assertIn("fail", response.data)
         self.assertEqual(Asset.objects.count(), count + 1)
+
+    @patch("api.authentication.auth.verify_id_token")
+    def test_download_non_existing_file_fails(self, mock_verify_id_token):
+
+        mock_verify_id_token.return_value = {"email": self.admin_user.email}
+
+        response = client.get(
+            f"{self.downloads_url}?filename=sample_import.xlsx",
+            HTTP_AUTHORIZATION=f"Token {self.token_admin}",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.data,
+            {"response": "No such file or directory as sample_import.xlsx"},
+        )
+
+    @patch("api.authentication.auth.verify_id_token")
+    def test_download_existing_file_succeeds(self, mock_verify_id_token):
+
+        mock_verify_id_token.return_value = {"email": self.admin_user.email}
+
+        response = client.get(
+            f"{self.downloads_url}?filename=sample_import.csv",
+            HTTP_AUTHORIZATION=f"Token {self.token_admin}",
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.filename, "sample_import.csv")
