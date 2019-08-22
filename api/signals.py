3cf76d2149067b046240d131f10dc1a05f1f7fea
Gitau <fgkinus@gmail.com>
2019-08-22 13:09:41 +0300

    ft-add-notifications: Add notifications model
    -  Add notifications model
    - add signals to create notifications
    - add unit tests
    - configure django_q and django_cache
    - resolve merge conflicts
    
    [Delivers #161678177]

--- api/signals.py
+++ api/signals.py
@@ -1,11 +1,24 @@
+"""
+A module containing signals used to listen to specific events in the models
+isort:skip_file
+"""
 # Third-Party Imports
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
+from rest_framework.reverse import reverse
 
 # App Imports
 from api.requestMiddleware import RequestMiddleware
 from core.constants import TABLES
 from core.models import History
+from core import constants
+from core.models import (  # isort:skip
+    AssetIncidentReport,
+    History,
+    Notifications,
+    StateTransition,
+    User,
+)
 
 
 @receiver(post_save)
@@ -30,3 +43,61 @@ def track_application_actions(sender, instance, **kwargs):
             history.save()
         except ValueError:
             pass
+
+
+@receiver(post_save, sender=AssetIncidentReport)
+def create_notification_on_incident_report_submission(**kwargs):
+    """
+    Create a notification to admin users when a user creates an incident report
+    :param kwargs:
+    :return:
+    """
+    instance = kwargs["instance"]
+    # if a new notification has been  created
+    if kwargs.get("created", False) is True:
+        notification_body = "{} submitted a new incident report {}."
+        notification_title = "New Incident Report"
+        notification_targets = User.objects.filter(is_superuser=True)
+        notification_origin = instance.submitted_by
+        incident_url = "{}/{}".format(reverse("incidence-reports-list"), instance.id)
+
+        # create notifications to te different admins
+        for target in notification_targets:
+            notification = Notifications(
+                title=notification_title,
+                body=notification_body.format(target.email, incident_url),
+                level=constants.INFO_NOTIFICATION,
+                origin=notification_origin,
+                target=target,
+            )
+            notification.save()
+            # send email to user
+
+
+@receiver(post_save, sender=StateTransition)
+def alert_user_when_incident_report_status_is_updated(**kwargs):
+    """
+    When the status of the incident report is modified, alert the user that filed the report
+    :param kwargs:
+    :return:
+    """
+    instance = kwargs["instance"]
+    # notification details
+    incident_url = "{}/{}".format(reverse("incidence-reports-list"), instance.id)
+    notification_body = "The status of incident report {} that you filed has been updated to `{}`.".format(
+        incident_url, instance.incident_report_state
+    )
+    notification_title = "Incident Report Status Update"
+    incident_report = AssetIncidentReport.objects.get(
+        id=instance.asset_incident_report.id
+    )
+    if incident_report.submitted_by is not None:
+        notification_target = incident_report.submitted_by
+        # create notification
+        notification = Notifications(
+            title=notification_title,
+            body=notification_body,
+            level=constants.INFO_NOTIFICATION,
+            target=notification_target,
+        )
+        notification.save()
--- core/tests/test_asset_incident_report_model.py
+++ core/tests/test_asset_incident_report_model.py
@@ -1,4 +1,9 @@
+# Third-Party Imports
+from django.apps import apps
+from rest_framework.reverse import reverse
+
 # App Imports
+from core import constants
 from core.tests import CoreBaseTestCase
 
 from ..models import AssetIncidentReport
@@ -23,3 +28,48 @@ class AssetIncidentReportModelTest(CoreBaseTestCase):
             str(incident_report),
             f"{incident_report.incident_type}: {incident_report.asset}",
         )
+
+
+class TestAssetIncidentReportNotifications(CoreBaseTestCase):
+    """
+    Test Notifications elicited by Asset Incident Report creation
+    """
+
+    # Asset_model = apps.get_model("core", "Asset")
+    User_model = apps.get_model("core", "User")
+    Notifications_model = apps.get_model("core", "Notifications")
+
+    def setUp(self):
+        self.admin_user = self.User_model.objects.create(
+            email="newuser@andela.com",
+            password="devpassword",
+            first_name="first",
+            last_name="second",
+            phone_number="254720900900",
+            location=self.centre,
+            department=self.department,
+            is_securityuser=True,
+            is_superuser=True,
+        )
+
+    def test_admins_are_notified_when_notification_is_created(self):
+        original_notifications_count = self.Notifications_model.objects.count()
+        admins = self.User_model.objects.filter(is_superuser=True)
+        admin_count = admins.count()
+        report = AssetIncidentReport.objects.create(
+            asset=self.test_asset, incident_type=constants.LOSS, submitted_by=self.user
+        )
+        notifications = self.Notifications_model.objects.filter(target__in=admins)
+        notification = notifications[0]
+        incident_url = "{}/{}".format(reverse("incidence-reports-list"), report.id)
+        body = "{} submitted a new incident report {}.".format(
+            self.admin_user.email, incident_url
+        )
+
+        self.assertEqual(
+            admin_count, notifications.count() - original_notifications_count
+        )
+        self.assertEqual(notification.origin, report.submitted_by)
+        self.assertEqual(notification.title, "New Incident Report")
+
+        self.assertEqual(notification.body, body)
